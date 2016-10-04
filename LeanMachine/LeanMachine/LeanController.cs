fdda9b7ccf619e39099a6f7c455c18882a5794ed
satyadeepthi <satyadeepthi.g@gmail.com>
2016-10-04 13:09:37 -0500

    Added files for project

---
+++ LeanMachine/LeanMachine/LeanController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeanMachine
+{
+    public class LeanController
+    {
+        private readonly List<Block> blocks;
+
+        public LeanController()
+        {
+            blocks = new List<Block>();
+        }
+
+        public void AddBlock(int capacity)
+        {
+
+            Queue queue = blocks.Any() ? new Queue((CalculateThroughput() - capacity) < 0 ? 0 : (CalculateThroughput() - capacity)) : new Queue(0);
+
+            blocks.Add(new Block(capacity, queue));
+        }
+
+        
+        public void Reset()
+        {
+            blocks.Clear();
+        }
+
+        public int CalculateThroughput()
+        {
+            if (HopNumber <= blocks.Count)
+            {
+                return 0;
+            }
+
+            return blocks.Select(x => x.Capacity).Min();
+        }
+
+        public int HopNumber { get; set; }
+
+        public void PrintSystem()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Print System:");
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                Console.WriteLine("Block Index: " + (i+1) + "\nBlock Capacity :" + blocks[i].Capacity);
+                Console.WriteLine("Queue size: " + blocks[i].Queue.Value);
+            }
+            Console.WriteLine("Throughput: " + CalculateThroughput());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LeanMachine/LeanMachine/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeanMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leanController = new LeanController();
+            while (true)
+            {
+                Console.WriteLine("Press 'a' to add a box");
+                var result = Console.ReadLine();
+                if (result.Trim().Equals("a"))
+                {
+                    Console.WriteLine("Enter the capacity: ");
+                    var capacity = Int16.Parse(Console.ReadLine());
+                    leanController.AddBlock(capacity);
+                }
+
+                leanController.PrintSystem();
+            }
+
+
+        }
+
+
+    }
+}
